5a0f27b9a76e2f14991b9f0860c8acf682f74e67
William957-web <85293841+William957-web@users.noreply.github.com>
2023-12-30 15:30:02 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<h1>Hello, <span><sapn id="Outcome"></sapn></h1>
+<img src='titan.jfif'>
+<script>
+  var getUrlString = location.href;
+  var url = new URL(getUrlString);
+  const Outcome=document.querySelector('#Outcome');
+  Outcome.innerHTML=url.searchParams.get('name');
+</script>
